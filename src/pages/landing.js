7dc4bb4f9f933051bc0f65e4bd0417456c06c80c
Mohan-I <mohanshyadav@gmail.com>
2026-01-20 11:33:54 +0530

    reupdates

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -122,17 +122,6 @@ const landing = () => {
     </div>
   </div>
 </div>
-<Card className="text-center">
-      <Card.Header>Featured</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
-    </Card>
 
 <br />
 
